3b50877646be2b7d45893e04a89126c141fffe7e
adil488444 <engadiljavedbhutta@gmail.com>
2025-10-24 18:28:35 +0500

    app is done

---
+++ app/src/main/java/com/example/imagefromcamera/ViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.imagefromcamera
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel : ViewModel() {
+    private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+    val bitmaps: StateFlow<List<Bitmap>> = _bitmaps
+    fun onTakePhoto(bitmap: Bitmap) {
+        _bitmaps.value = _bitmaps.value + bitmap
+    }
+
+}
+
